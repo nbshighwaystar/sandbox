6e4c8f597e8420357199b73e7be2994fac8e16fa
Neil Sambo <49774161+nbshighwaystar@users.noreply.github.com>
2019-05-30 15:33:29 +0800

    modified function.php and manage_content

--- widget_corp/includes/function.php
+++ widget_corp/includes/function.php
@@ -9,20 +9,26 @@
 
     function find_all_subjects()
     {
+        global $connection;
         $query  = "SELECT * ";
         $query .= "FROM subjects ";
         $query .= "WHERE visible = 1 ";
         $query .= "ORDER BY id ASC";
-        return $query; 
+        $subjet_set = mysqli_query($connection, $query);
+        confirm_query($subjet_set);
+        return $subjet_set; 
     }
 
-    function find_page_by_id($subject)
+    function find_pages_for_subject($subject_id)
     {
+        global $connection;
         $query  = "SELECT * ";
         $query .= "FROM pages ";
         $query .= "WHERE visible = 1 ";
-        $query .= "AND subject_id = {$subject["id"]} ";
-        $query .= "ORDER BY id ASC";
-        return $query;
+        $query .= "AND subject_id = {$subject_id} ";
+        $query .= "ORDER BY position ASC";
+        $page_set = mysqli_query($connection, $query);
+        confirm_query($page_set);
+        return $page_set;
     }
-?>
+?> 
--- widget_corp/public/manage_content.php
+++ widget_corp/public/manage_content.php
@@ -2,34 +2,36 @@
 <?php require_once("../includes/db_connection.php"); ?>
 <?php require_once("../includes/function.php"); ?>
 <?php include("../includes/layouts/header.php"); ?>
+
+<?php
+    if (isset($_GET["subject"]))
+    {
+        $selected_subject_id = $_GET["subject"];
+    } elseif (isset($_GET["page"])) {
+        $selected_page_id = $_GET["page"];
+    }
+?>
     <div id="main">
         <div id="navigation">
         <ul class="subjects">
 
-        <?php 
-            
-            $subject_set = mysqli_query($connection, find_all_subjects());
-            confirm_query($subject_set);
-        ?>
+        <?php $subject_set = find_all_subjects();   ?>
         <?php
             while ($subject=mysqli_fetch_assoc($subject_set)) {
                 ?>        
-                <li><?php echo $subject['menu_name']; ?>
-                <?php 
-                        
-                        $page_set = mysqli_query($connection, find_all_pages());
-                        confirm_query($page_set);
-                        ?>
+                <li>
+                    <a href="manage_content.php?subject=<?php echo urlencode($subject["id"]); ?>"><?php echo $subject['menu_name']; ?></a>
+                    <?php $page_set = find_pages_for_subject($subject["id"]) ?>
                         
                     <ul class=pages>
-                    <?php
-                        while ($page=mysqli_fetch_assoc($page_set)) {
-                           ?>
-                            <li><?php echo $page['menu_name']; ?></li>
+                        <?php
+                            while ($page=mysqli_fetch_assoc($page_set)) {
+                        ?>
+                            <li><a href="manage_content.php?page=<?php echo urlencode($page["id"]); ?>"><?php echo $page['menu_name']; ?></a></li>
                         <?php } ?>
                     <?php mysqli_free_result($page_set); ?>
                     </ul>  
-                    </li>
+                </li>
         <?php }  ?>
         </ul>
         <?php 
@@ -40,6 +42,9 @@
         </div>
             <div id="page">
                 <h2>Manage Content</h2>
+                <?php echo $selected_subject_id; ?>
+                <?php echo $selected_page_id; ?>
+
             </div>
     </div>
 
