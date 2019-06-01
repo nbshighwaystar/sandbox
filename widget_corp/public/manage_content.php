de88366155fd616117742e3ec2394a60cbdccce8
Neil Sambo <49774161+nbshighwaystar@users.noreply.github.com>
2019-06-01 21:38:06 +0800

    added default values on selected_subject_id and selected_page_id

--- widget_corp/public/manage_content.php
+++ widget_corp/public/manage_content.php
@@ -7,8 +7,14 @@
     if (isset($_GET["subject"]))
     {
         $selected_subject_id = $_GET["subject"];
+        $selected_page_id = null;
     } elseif (isset($_GET["page"])) {
         $selected_page_id = $_GET["page"];
+        $selected_subject_id = null;
+    }else 
+    {
+        $selected_subject_id = null;
+        $selected_page_id = null;
     }
 ?>
     <div id="main">
