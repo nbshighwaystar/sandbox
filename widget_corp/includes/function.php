44df82f0dc647ddaaa94dc01ec9fdcf87b733ee4
Neil Sambo <49774161+nbshighwaystar@users.noreply.github.com>
2019-06-02 17:40:41 +0800

    Improve manage_content function pages.

--- widget_corp/includes/function.php
+++ widget_corp/includes/function.php
@@ -12,32 +12,73 @@
         global $connection;
         $query  = "SELECT * ";
         $query .= "FROM subjects ";
-        $query .= "WHERE visible = 1 ";
+     //   $query .= "WHERE visible = 1 ";
         $query .= "ORDER BY id ASC";
-        $subjet_set = mysqli_query($connection, $query);
-        confirm_query($subjet_set);
-        return $subjet_set; 
+        $subject_set = mysqli_query($connection, $query);
+        confirm_query($subject_set);
+        return $subject_set; 
     }
 
     function find_pages_for_subject($subject_id)
     {
         global $connection;
+        $safe_subject_id = mysqli_real_escape_string($connection,$subject_id);
+
         $query  = "SELECT * ";
         $query .= "FROM pages ";
         $query .= "WHERE visible = 1 ";
-        $query .= "AND subject_id = {$subject_id} ";
+        $query .= "AND subject_id = {$safe_subject_id} ";
         $query .= "ORDER BY position ASC";
         $page_set = mysqli_query($connection, $query);
         confirm_query($page_set);
         return $page_set;
     }
 
+    function find_subject_by_id($subject_id)
+    {
+        global $connection;
+        $safe_subject_id = mysqli_real_escape_string($connection, $subject_id);
+        $query  = "SELECT * ";
+        $query .= " FROM subjects ";
+        $query .= " WHERE id = {$safe_subject_id} ";
+        $query .= " LIMIT  1";
+        $subject_set = mysqli_query($connection, $query);
+        confirm_query($subject_set);
+        if($subject = mysqli_fetch_assoc($subject_set))
+        {
+            return $subject; 
+        } else 
+        {
+            return null;
+        }
+    }
+
+    function find_page_by_id($page_id) 
+    {
+       global $connection;
+
+       $safe_page_id = mysqli_real_escape_string($connection, $page_id);
+       $query  = "SELECT * ";
+        $query .= "FROM pages ";
+        $query .= "WHERE id = {$safe_page_id} ";
+        $query .= "LIMIT  1";
+        $page_set = mysqli_query($connection, $query);
+        confirm_query($page_set);
+        if($page = mysqli_fetch_assoc($page_set))
+        {
+            return $page; 
+        } else 
+        {
+            return null;
+        }
+    }
+
     function navigation($subject_id, $page_id)
     {   
        
         $output = "<ul class=\"subjects\">";
-        $subjet_set = find_all_subjects();
-        while ($subject=mysqli_fetch_assoc($subjet_set)) 
+        $subject_set = find_all_subjects();
+        while ($subject=mysqli_fetch_assoc($subject_set)) 
         {
             $output .= "<li ";
                 if ($subject_id == $subject['id']) 
@@ -74,7 +115,7 @@
             mysqli_free_result($page_set);
         $output .="</ul>";
         return $output;
-        mysqli_free_result($subjet_set);
+        mysqli_free_result($subject_set);
         
     }
 ?> 
--- widget_corp/public/manage_content.php
+++ widget_corp/public/manage_content.php
@@ -25,17 +25,20 @@
 <a href="admin.php">Admin Page</a>
         </div>
             <div id="page">
-                <h2>Manage Content</h2>
-                <?php 
+               <?php 
                 if ($selected_subject_id)
                 { ?>
-                    <?php echo $selected_subject_id; ?>
+                <h2>Manage Subject</h2>
+                    <?php $current_subject = find_subject_by_id($selected_subject_id); ?>
+                    Menu name: <?php echo $current_subject["menu_name"]; ?> <br />
                 <?php } elseif ($selected_page_id)
                 { ?>
-                    <?php echo $selected_page_id; ?>
+                    <h2>Manage Page</h2>
+                    <?php $current_page = find_page_by_id($selected_page_id); ?>
+                    Menu name: <?php echo $current_page{"menu_name"}; ?> <br />
                 <?php } else 
                 { ?>
-                    Please select a subject or a page.
+                    <h2>Please select a subject or a page.</h2>
                 <?php } ?>
 
             </div>
