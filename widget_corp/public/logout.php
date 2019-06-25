f26396f877d2ced4ac8d2e371b5b494477dfe83a
Neil Sambo <49774161+nbshighwaystar@users.noreply.github.com>
2019-06-25 18:37:54 +0800

    added the logout page. (FINAL)

---
+++ widget_corp/public/logout.php
@@ -0,0 +1,24 @@
+<?php ob_start(); ?>
+<?php require_once("../includes/session.php"); ?>
+<?php require_once("../includes/function.php"); ?>
+
+<?php
+    // v1: simple logout
+    // session_start();
+    $_SESSION["admin_id"] = null;
+    $_SESSION["username"] = null;
+    redirect_to("login.php");
+?>
+
+<?php
+    // v2: destroy session
+    // assumes nothing else in session to keep
+    /* session_start();
+    $_SESSION = array();
+    if (isset($_COOKIE[session_name()]))
+    {
+        setcookie(session_name(), '', time()-42000, '/');
+    }
+    session_destroy();
+    redirect_to("login.php") */
+?>
